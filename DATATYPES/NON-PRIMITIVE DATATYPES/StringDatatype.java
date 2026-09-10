5848b328bbc0120f01b0978ca321dc23bba61023
amolpawar24 <amolpawar.me@gmail.com>
2026-09-10 18:10:05 +0530

    Add String datatype example

---
+++ DATATYPES/NON-PRIMITIVE DATATYPES/StringDatatype.java
@@ -0,0 +1,16 @@
+class StringDatatype 
+{
+	public static void main(String[] args) 
+	{
+		String str1 = "Hello";
+		String str2 = "World";
+		System.out.println(str1);
+		System.out.println(str2);
+	}
+}
+/*
+==> It is Non-Primitive Datatype in Java.
+==> String is a Class in Java.
+==>We can create the string in two ways.
+	1.Literals	2.Using String Constructor
+*/
