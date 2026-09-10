1baf6cc3a9508be2a9f6aae9709d92df900940fd
amolpawar24 <amolpawar.me@gmail.com>
2026-09-10 18:08:59 +0530

    Add double datatype example

---
+++ DATATYPES/PRIMITIVE DATATYPES/DoubleDatatype.java
@@ -0,0 +1,19 @@
+class DoubleDatatype 
+{
+	public static void main(String[] args) 
+	{
+		//d is optinal you can declare or not
+		double a = 367.347846343d;
+		
+		System.out.println(a);
+
+	}
+}
+
+/* Range : upto 16 decimal digits */
+/* Size : 64bits = 8byte */
+/* Accepts :
+==>	1.23456e300d
+==>	-123456e-300d
+==> 1e1d
+*/
