5c77630aaa17f0df5d576648014920fddc26b52f
amolpawar24 <amolpawar.me@gmail.com>
2026-09-10 18:07:12 +0530

    Add float datatype example

---
+++ DATATYPES/PRIMITIVE DATATYPES/FloatDatatype.java
@@ -0,0 +1,19 @@
+class FloatDatatype 
+{
+	public static void main(String[] args) 
+	{
+		// The f is mandatory when you assign decimal value with float datatype
+		float a = 75.33f;
+		float b = 1.23100f;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
+
+/* Range : upto 7 decimal digits
+/* Size = 32bits = 4 byte
+==> 1.23100f
+==> -1.23e-100f
+==> .3f
+==> 3.14F
+*/
