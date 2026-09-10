6eb4460d12b58d2ea124a25aa3ab4fad518776ca
amolpawar24 <amolpawar.me@gmail.com>
2026-09-10 18:06:16 +0530

    Add long datatype example

---
+++ DATATYPES/PRIMITIVE DATATYPES/LongDatatype.java
@@ -0,0 +1,18 @@
+class LongDatatype
+{
+	public static void main(String[] args) 
+	{
+		long a=7457553545l;
+		long b=874354674345l;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
+/* Range : -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 */
+/* Accepts :
+==> -2L
+==> -1L
+==> 0L
+==> 1L
+==> 2L
+*/
