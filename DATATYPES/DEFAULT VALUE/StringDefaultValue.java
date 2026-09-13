86141ca11e32733907b7c24e4616b96cd6eb6f16
amolpawar24 <amolpawar.me@gmail.com>
2026-09-13 17:39:24 +0530

    Add String default value example

---
+++ DATATYPES/DEFAULT VALUE/StringDefaultValue.java
@@ -0,0 +1,11 @@
+class StringDefaultValue
+{
+	static String str;
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("str : "+str);
+	}
+}
+
+// string Default Value is ===> null
