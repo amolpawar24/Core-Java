9d9e04b99bfc855ce968576ad7e08298fdebb07a
amolpawar24 <amolpawar.me@gmail.com>
2026-09-15 20:47:59 +0530

    Add ArithematicOperators class to demonstrate basic arithmetic operations in Java

---
+++ OPERATORS/BINARY OPERATORS/ARITHEMATIC OPERATORS/ArithematicOperators.java
@@ -0,0 +1,15 @@
+class ArithematicOperators 
+{
+	public static void main(String[] args) 
+	{
+		
+		int a=2;
+		int b=2;
+		System.out.println(a+b);//4
+		System.out.println(a-b);//0
+		System.out.println(a*b);//4
+		System.out.println(a/b);//1
+		System.out.println(a%b);//0
+
+	}
+}
