38967526a34964e1f9fe09d1585e8361831a53c6
amolpawar24 <amolpawar.me@gmail.com>
2026-09-15 20:45:15 +0530

    Add Print class demonstrating basic printing with System.out.print

---
+++ JAVA PRINTING STATEMENTS/PRINT()/Print.java
@@ -0,0 +1,11 @@
+class Print 
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("Hey...");
+		System.out.print(" Hello Everyone");
+	}
+}
+
+/* print()
+==>It is used to print the value and move the cursor to same line.
