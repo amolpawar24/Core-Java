4595f069495aa4f7511c37ea35454d8d6c16f2a7
amolpawar24 <amolpawar.me@gmail.com>
2026-09-15 20:48:08 +0530

    Add ANDOperator class to demonstrate logical AND operations in Java

---
+++ OPERATORS/BINARY OPERATORS/LOGICAL OPERATORS/AND OPERATOR[ && ]/ANDOperator.java
@@ -0,0 +1,10 @@
+class ANDOperator 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(true && true);//true
+		System.out.println(true && false);//false
+		System.out.println(false && true);//false
+		System.out.println(false && false);//false
+	}
+}
