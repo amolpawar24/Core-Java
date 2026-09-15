78ca392de1037c86d7a5f66e4f4162fd037906cb
amolpawar24 <amolpawar.me@gmail.com>
2026-09-15 20:44:57 +0530

    Add Addition class demonstrating arithmetic operations with different data types

---
+++ [ + ] OPERATOR/ADDITION/Addition.java
@@ -0,0 +1,16 @@
+class Addition 
+{
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		float b = 23.24f;
+		double c = 23223;
+		long d = 87444;
+		byte e = 124;
+		short f = 34;
+
+		System.out.println(a+b);
+		System.out.println(c+d);
+		System.out.println(e+f);
+	}
+}
