d5bd8cf289dbcd5c78dc069e83d79aaa37aa43a7
amolpawar24 <amolpawar.me@gmail.com>
2026-09-15 20:45:24 +0530

    Add Println class demonstrating basic usage of System.out.println

---
+++ JAVA PRINTING STATEMENTS/PRINTLN()/Println.java
@@ -0,0 +1,10 @@
+class Println
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hey...");
+		System.out.println("Hello Everyone");
+	}
+}
+
+/* It is used to print the value and Move the Cursor to the Next Line.*/
